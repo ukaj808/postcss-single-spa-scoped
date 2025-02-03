3c7c68dca4509a753b03d5858d3ba976c33c474a
Fisnik Ukaj <fisnik.ukaj@justeattakeaway.com>
2025-02-03 13:04:25 +0100

    Replace :root selector with prefix

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const uuid = require('uuid');
 
 
 const extractPseudoElement = (str) => {
-    const pseudoElementsRegex = /^(.*?)(::(?:after|backdrop|before|cue(?:\([^()]*\))?|file-selector-button|first-letter|first-line|grammar-error|highlight\([^()]+\)|marker|part\([^()]+\)|placeholder|selection|slotted\([^()]+\)|spelling-error|target-text|view-transition(?:-image-pair\([^()]+\)|-group\([^()]+\)|-new\([^()]+\)|-old\([^()]+\))?))$/;
-    const match = str.match(pseudoElementsRegex);
-    if (match) {
-        return {
-            selector: match[1],         // The string before the pseudo-element
-            pseudoElement: match[2], // The matched pseudo-element
-        };
-    }
-    return null;
+  const pseudoElementsRegex = /^(.*?)(::(?:after|backdrop|before|cue(?:\([^()]*\))?|file-selector-button|first-letter|first-line|grammar-error|highlight\([^()]+\)|marker|part\([^()]+\)|placeholder|selection|slotted\([^()]+\)|spelling-error|target-text|view-transition(?:-image-pair\([^()]+\)|-group\([^()]+\)|-new\([^()]+\)|-old\([^()]+\))?))$/;
+  const match = str.match(pseudoElementsRegex);
+  if (match) {
+    return {
+      selector: match[1],         // The string before the pseudo-element
+      pseudoElement: match[2], // The matched pseudo-element
+    };
+  }
+  return null;
 }
 /**
  * @typedef {Object} PluginOpts
@@ -36,7 +36,7 @@ module.exports = (opts) => {
     let pjson;
     try {
       if (process.env.NODE_ENV === 'test') {
-        pjson = { name: '@org/app-name' } ;
+        pjson = { name: '@org/app-name' };
       } else {
         pjson = require(`${process.cwd()}/package.json`);
       }
@@ -57,105 +57,107 @@ module.exports = (opts) => {
   const processed = Symbol('processed');
 
   return {
-   postcssPlugin: 'postcss-single-spa-scoped',
-   AtRule (atRule) {
+    postcssPlugin: 'postcss-single-spa-scoped',
+    AtRule(atRule) {
       if (atRule[processed]) return;
       if (atRule.name === 'keyframes') {
         atRule.params = atRule.params + keyframeSuffix;
         atRule[processed] = true;
       }
-   },
-   Declaration (decl) {
+    },
+    Declaration(decl) {
       if (decl[processed]) return;
       if (decl.prop === 'animation-name') {
         decl.value = decl.value + keyframeSuffix;
         decl[processed] = true;
       }
-   },
-   Rule (rule) {
+    },
+    Rule(rule) {
 
 
-    // -1. Check if rule is a keyframes rule (AND DONT PREFIX!)
-    // This list is subject to grow... I'm unsure of all the use cases.
-    if (rule.parent && rule.parent.type === 'atrule' && rule.parent.name === 'keyframes') {
-      rule[processed] = true;
-      return;
-    }
-
-    // 0. Check if rule has already been processed
-    if (rule[processed]) return;
-
-    // 1. Check if rule has a comment above it to ignore
-    if (rule.prev() && rule.prev().type === 'comment') {
-      const comment = rule.prev();
-      if (comment.text === 'single-spa-prefix-ignore') {
+      // -1. Check if rule is a keyframes rule (AND DONT PREFIX!)
+      // This list is subject to grow... I'm unsure of all the use cases.
+      if (rule.parent && rule.parent.type === 'atrule' && rule.parent.name === 'keyframes') {
         rule[processed] = true;
         return;
       }
-    }
 
-    // SVELTE HACK
-    if (rule.source && rule.source.input && rule.source.input.file) {
-      const filePath = path.parse(rule.source.input.file);
-      if (filePath.base && filePath.base.includes('.vite-preprocess.')) {
-        rule[processed] = true;
-        return;
+      // 0. Check if rule has already been processed
+      if (rule[processed]) return;
+
+      // 1. Check if rule has a comment above it to ignore
+      if (rule.prev() && rule.prev().type === 'comment') {
+        const comment = rule.prev();
+        if (comment.text === 'single-spa-prefix-ignore') {
+          rule[processed] = true;
+          return;
+        }
       }
-    }
 
-    const additionalSelectorsProvided = opts.additionalSelectors && opts.additionalSelectors.length > 0;
-
-    // sanitize additional selectors
-    if (additionalSelectorsProvided) {
-      opts.additionalSelectors = opts.additionalSelectors.filter(s => s !== null && s.length > 0).map(s => s.trim());
-    }
-
-    rule.selector = rule.selectors.reduce((resultSelector, selector, i) => {
-      let suffix = i === rule.selectors.length - 1 ? '' : ', ';
-      let additionalSelectors = additionalSelectorsProvided ? ', ' +  opts.additionalSelectors.map(s =>  `${s} ${selector}`).join(', ') : '';
+      // SVELTE HACK
+      if (rule.source && rule.source.input && rule.source.input.file) {
+        const filePath = path.parse(rule.source.input.file);
+        if (filePath.base && filePath.base.includes('.vite-preprocess.')) {
+          rule[processed] = true;
+          return;
+        }
+      }
 
 
-      // 3. Check if selector is already prefixed
-      if (selector.startsWith(prefix)) {
-        if (opts.excludeParcels) {
-          const selectorWithoutPrefix = selector.substring(prefix.length + 1); // +1 for the hierarchy space
-          const pseudoElement = extractPseudoElement(selectorWithoutPrefix);
+      const additionalSelectorsProvided = opts.additionalSelectors && opts.additionalSelectors.length > 0;
 
-          if (pseudoElement) {
+      // sanitize additional selectors
+      if (additionalSelectorsProvided) {
+        opts.additionalSelectors = opts.additionalSelectors.filter(s => s !== null && s.length > 0).map(s => s.trim());
+      }
+
+      rule.selector = rule.selectors.reduce((resultSelector, selector, i) => {
+        let suffix = i === rule.selectors.length - 1 ? '' : ', ';
+        let additionalSelectors = additionalSelectorsProvided ? ', ' + opts.additionalSelectors.map(s => `${s} ${selector}`).join(', ') : '';
+
+        selector = selector.replaceAll(":root", prefix);
+
+        // 3. Check if selector is already prefixed
+        if (selector.startsWith(prefix)) {
+          if (opts.excludeParcels) {
+            const selectorWithoutPrefix = selector.substring(prefix.length + 1); // +1 for the hierarchy space
+            const pseudoElement = extractPseudoElement(selectorWithoutPrefix);
+
+            if (pseudoElement) {
+              if (!pseudoElement.selector) pseudoElement.selector = '*';
+              const excludeParcelsPseudoClass = `:not([id^="single-spa-application\\:parcel"] ${pseudoElement.selector})`;
+              return resultSelector + prefix + ' ' + pseudoElement.selector + excludeParcelsPseudoClass + pseudoElement.pseudoElement + suffix;
+            }
+
+            const excludeParcelsPseudoClass = `:not([id^="single-spa-application\\:parcel"] ${selectorWithoutPrefix})`;
+            return resultSelector + selector + excludeParcelsPseudoClass + suffix;
+          }
+          return resultSelector + selector + suffix;
+        }
+
+        const pseudoElement = extractPseudoElement(selector);
+
+        if (pseudoElement) {
+          if (opts.excludeParcels) {
             if (!pseudoElement.selector) pseudoElement.selector = '*';
             const excludeParcelsPseudoClass = `:not([id^="single-spa-application\\:parcel"] ${pseudoElement.selector})`;
-            return resultSelector + prefix + ' ' + pseudoElement.selector + excludeParcelsPseudoClass + pseudoElement.pseudoElement + suffix;
+            return resultSelector + prefix + ' ' + pseudoElement.selector + excludeParcelsPseudoClass + pseudoElement.pseudoElement + additionalSelectors + suffix;
           }
-
-          const excludeParcelsPseudoClass = `:not([id^="single-spa-application\\:parcel"] ${selectorWithoutPrefix})`;
-          return resultSelector + selector + excludeParcelsPseudoClass + suffix;
         }
-        return resultSelector + selector + suffix;
-      }
 
-      const pseudoElement = extractPseudoElement(selector);
+        // 4. Prefix selector
+        return resultSelector
+          + prefix + ' '
+          + selector
+          + (opts.excludeParcels ? `:not([id^="single-spa-application\\:parcel"] ${selector})` : '')
+          + additionalSelectors
+          + suffix;
 
-      if (pseudoElement) {
-        if (opts.excludeParcels) {
-          if (!pseudoElement.selector) pseudoElement.selector = '*';
-          const excludeParcelsPseudoClass = `:not([id^="single-spa-application\\:parcel"] ${pseudoElement.selector})`;
-          return resultSelector + prefix + ' ' + pseudoElement.selector + excludeParcelsPseudoClass + pseudoElement.pseudoElement + additionalSelectors + suffix;
-        }
-      }
+      }, '');
 
-      // 4. Prefix selector
-      return resultSelector
-        + prefix + ' '
-        + selector
-        + (opts.excludeParcels ? `:not([id^="single-spa-application\\:parcel"] ${selector})` : '')
-        + additionalSelectors
-        + suffix;
+      rule[processed] = true;
 
-    }, '');
-
-    rule[processed] = true;
-
-   },
+    },
   }
 }
 
--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ const plugin = require('./');
 
 jest.mock('uuid');
 
-async function run (input, output, opts = { }) {
+async function run(input, output, opts = {}) {
   let result = await postcss([plugin(opts)]).process(input, { from: undefined })
   expect(result.css).toMatch(output)
   expect(result.warnings()).toHaveLength(0)
@@ -16,29 +16,29 @@ async function run (input, output, opts = { }) {
 
 it('rules already prefixed with the single-spa prefix are ignored', async () => {
   await run('#single-spa-application\\:\\@org\\/app-name { };',
-            '#single-spa-application\\:\\@org\\/app-name { };');
+    '#single-spa-application\\:\\@org\\/app-name { };');
 });
 
 it('"single-spa-prefix-ignore comment prefixed" rule is ignored', async () => {
   await run('/* single-spa-prefix-ignore */\na { }; /* single-spa-prefix-ignore */.c1 { };',
-            '/* single-spa-prefix-ignore */\na { }; /* single-spa-prefix-ignore */.c1 { };');
+    '/* single-spa-prefix-ignore */\na { }; /* single-spa-prefix-ignore */.c1 { };');
 });
 
 it('rule prefixed with comment other than single-spa-prefix-ignore is prefixed', async () => {
   await run('/* single-spa-prefix-ignore */\na { }; /* single-spa-prefix-ignore */.c1 { }; /* this class is nice! */.c2 { };',
-            '/* single-spa-prefix-ignore */\na { }; /* single-spa-prefix-ignore */.c1 { }; /* this class is nice! */#single-spa-application\\:\\@org\\/app-name .c2 { };');
+    '/* single-spa-prefix-ignore */\na { }; /* single-spa-prefix-ignore */.c1 { }; /* this class is nice! */#single-spa-application\\:\\@org\\/app-name .c2 { };');
 });
 
 it('plugin options appName is used as the prefix', async () => {
   await run('a { };',
-            '#single-spa-application\\:my-app a { };',
-            { appName: 'my-app' });
+    '#single-spa-application\\:my-app a { };',
+    { appName: 'my-app' });
 });
 
 it('Prefix with additional selectors ontop of single-spa prefix, if additional selectors provided', async () => {
   await run('a { display: flex; };',
-            '#single-spa-application\\:\\@org\\/app-name a, #app a, #blahblah123 a { display: flex; };',
-            { additionalSelectors: ['#app', '#blahblah123'] });
+    '#single-spa-application\\:\\@org\\/app-name a, #app a, #blahblah123 a { display: flex; };',
+    { additionalSelectors: ['#app', '#blahblah123'] });
 });
 
 it('rules already prefixed with the single-spa prefix are ignored and additional selectors are ignored', async () => {
@@ -55,68 +55,68 @@ it('selector list with one already prefixed selector correctly only prefixes the
 
 it('Filter out empty string additinal selectors', async () => {
   await run('a { display: flex; };',
-            '#single-spa-application\\:\\@org\\/app-name a, #app a { display: flex; };',
-            { additionalSelectors: ['#app', ''] });
+    '#single-spa-application\\:\\@org\\/app-name a, #app a { display: flex; };',
+    { additionalSelectors: ['#app', ''] });
 });
 
 it('Empty additionalSelectors works the same as not providing additionalSelectors', async () => {
   await run('a { display: flex; };',
-            '#single-spa-application\\:\\@org\\/app-name a { display: flex; };',
-            { additionalSelectors: [] });
+    '#single-spa-application\\:\\@org\\/app-name a { display: flex; };',
+    { additionalSelectors: [] });
 });
 
 
 it('Rule with descendant selector is prefixed correctly', async () => {
   await run('div a { display: flex; };',
-            '#single-spa-application\\:\\@org\\/app-name div a { display: flex; };');
+    '#single-spa-application\\:\\@org\\/app-name div a { display: flex; };');
 });
 
 it('Rule with multiple descendant selectors is prefixed correctly', async () => {
   await run('div a .c1 { display: flex; };',
-            '#single-spa-application\\:\\@org\\/app-name div a .c1 { display: flex; };');
+    '#single-spa-application\\:\\@org\\/app-name div a .c1 { display: flex; };');
 });
 
 it('Rule with different combinators is prefixed correctly', async () => {
   await run('div > a + .c1 { display: flex; };',
-            '#single-spa-application\\:\\@org\\/app-name div > a + .c1 { display: flex; };');
+    '#single-spa-application\\:\\@org\\/app-name div > a + .c1 { display: flex; };');
 });
 
 it('Rule with pseudo-element is prefixed correctly', async () => {
   await run('button::after { display: flex; };',
-            '#single-spa-application\\:\\@org\\/app-name button::after { display: flex; };');
+    '#single-spa-application\\:\\@org\\/app-name button::after { display: flex; };');
 });
 
 it('A rule with a list of selectors, each having a different combinator, is prefixed correctly', async () => {
   await run('div > a + .c1, div a .c2, div ~ .c3 { display: flex; };',
-            '#single-spa-application\\:\\@org\\/app-name div > a + .c1, #single-spa-application\\:\\@org\\/app-name div a .c2, #single-spa-application\\:\\@org\\/app-name div ~ .c3 { display: flex; };');
+    '#single-spa-application\\:\\@org\\/app-name div > a + .c1, #single-spa-application\\:\\@org\\/app-name div a .c2, #single-spa-application\\:\\@org\\/app-name div ~ .c3 { display: flex; };');
 });
 
 
 it('Grouped selectors are prefixed correctly, before each individual selector', async () => {
   await run('a, b, c { display: flex; };',
-            '#single-spa-application\\:\\@org\\/app-name a, #single-spa-application\\:\\@org\\/app-name b, #single-spa-application\\:\\@org\\/app-name c { display: flex; };');
+    '#single-spa-application\\:\\@org\\/app-name a, #single-spa-application\\:\\@org\\/app-name b, #single-spa-application\\:\\@org\\/app-name c { display: flex; };');
 
 });
 
 it('grouped selectors are prefixed correctly, before each individual selector, with additional selectors', async () => {
   await run('a, b, c { display: flex; };',
-            '#single-spa-application\\:\\@org\\/app-name a, #app a, #blahblah123 a, #single-spa-application\\:\\@org\\/app-name b, #app b, #blahblah123 b, #single-spa-application\\:\\@org\\/app-name c, #app c, #blahblah123 c { display: flex; };',
-            { additionalSelectors: ['#app', '#blahblah123'] });
+    '#single-spa-application\\:\\@org\\/app-name a, #app a, #blahblah123 a, #single-spa-application\\:\\@org\\/app-name b, #app b, #blahblah123 b, #single-spa-application\\:\\@org\\/app-name c, #app c, #blahblah123 c { display: flex; };',
+    { additionalSelectors: ['#app', '#blahblah123'] });
 });
 
 it('Keyframe names are suffixed and keyframe offset selectors are not prefixed', async () => {
   await run('@keyframes mymove { from { top: 0px; } to { top: 200px; } }',
-            '@keyframes mymove-b8f36a64 { from { top: 0px; } to { top: 200px; } }');
+    '@keyframes mymove-b8f36a64 { from { top: 0px; } to { top: 200px; } }');
 });
 
 it('Keyframes and all references are suffixed with the first chunk of a uid', async () => {
   await run('@keyframes mymove { from { top: 0px; } to { top: 200px; } }; .c1 { animation-name: mymove; }',
-            '@keyframes mymove-b8f36a64 { from { top: 0px; } to { top: 200px; } }; #single-spa-application\\:\\@org\\/app-name .c1 { animation-name: mymove-b8f36a64; }');
+    '@keyframes mymove-b8f36a64 { from { top: 0px; } to { top: 200px; } }; #single-spa-application\\:\\@org\\/app-name .c1 { animation-name: mymove-b8f36a64; }');
 });
 
 it('Additional :not pseudo-class suffixed to selectors to exclude styling of parcels', async () => {
   await run('a { display: flex; };',
-            '#single-spa-application\\:\\@org\\/app-name a:not([id^="single-spa-application\\:parcel"] a) { display: flex; };',
+    '#single-spa-application\\:\\@org\\/app-name a:not([id^="single-spa-application\\:parcel"] a) { display: flex; };',
     { excludeParcels: true });
 });
 
@@ -134,7 +134,7 @@ it('Additional :not pseudo-class contains the original selector (with combinator
 
 it('Rules already prefixed with the single-spa-prefix have there original selector suffixed with the exclude parcel pseudo class', async () => {
   await run('#single-spa-application\\:\\@org\\/app-name a .b { display: flex; };',
-            '#single-spa-application\\:\\@org\\/app-name a .b:not([id^="single-spa-application\\:parcel"] a .b) { display: flex; };',
+    '#single-spa-application\\:\\@org\\/app-name a .b:not([id^="single-spa-application\\:parcel"] a .b) { display: flex; };',
     { excludeParcels: true });
 });
 
@@ -211,5 +211,20 @@ it('When ::before or ::after are used on there own (implying all elements) and a
   await run('#single-spa-application\\:\\@org\\/app-name ::before , #single-spa-application\\:\\@org\\/app-name ::after { display: flex; };',
     '#single-spa-application\\:\\@org\\/app-name *:not([id^="single-spa-application\\:parcel"] *)::before, #single-spa-application\\:\\@org\\/app-name *:not([id^="single-spa-application\\:parcel"] *)::after { display: flex; };',
     { excludeParcels: true });
+});
+
+it(':root is replaced entirely with the single spa app id', async () => {
+  await run(':root { --main-bg-color: brown; };',
+    '#single-spa-application\\:\\@org\\/app-name { --main-bg-color: brown; }');
+});
+
+it(':root with combinator only replaces :root with single spa app id', async () => {
+  await run(':root > .c1 { --main-bg-color: brown; };',
+    '#single-spa-application\\:\\@org\\/app-name > .c1 { --main-bg-color: brown; }');
+});
+
+it(':root is replaced entirely with the single spa app id when in a list of selectors', async () => {
+  await run('button, :root { --main-bg-color: brown; };',
+    '#single-spa-application\\:\\@org\\/app-name button, #single-spa-application\\:\\@org\\/app-name { --main-bg-color: brown; }');
 });
 
